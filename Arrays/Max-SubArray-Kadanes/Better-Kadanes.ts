41dfdaebe597db9db00be0303a054f13afad7e45
zeeshan-kryptomind <zeeshan.ahmed@kryptomind.com>
2025-01-24 10:52:41 +0500

    Minor changes

--- Arrays/Max-SubArray-Kadanes/Better-Kadanes.ts
+++ Arrays/Max-SubArray-Kadanes/Better-Kadanes.ts
@@ -4,29 +4,39 @@ const numsArr: number[] = [-2, 1, -3, 4, -1, 2, 1, -5, 4]; // Input array of int
 
 function findMaxSubArr(numsArr: number[]) {
   let maxSum = Number.MIN_VALUE; // Initialize maxSum with the smallest possible value
-  let start = 0; // Variable to store the starting index of the maximum subarray
-  let end = 0; // Variable to store the ending index of the maximum subarray
+  let start = -1; // Variable to store the starting index of the maximum subarray
+  let ansStart = -1; // Variable to store the starting index of the maximum answer subarray
+  let ansEnd = -1; // Variable to store the ending index of the maximum answer subarray
+
   let intervalSum = 0; // Variable to store the sum of the current subarray
 
   // Iterate over the entire array to calculate maximum sum subarray
   for (let i = 0; i < numsArr.length; i++) {
+    if (intervalSum === 0) {
+      start = i; // Set the start to i means its a new subArray start
+    }
+
     intervalSum += numsArr[i]; // Add the current element to the subarray sum
 
     // If the current subarray sum exceeds the max sum, update maxSum and the end point
     if (intervalSum > maxSum) {
       maxSum = intervalSum; // Update the maximum sum
-      end = i; // Update the end index of the subarray
+      ansStart = start;
+      ansEnd = i; // Update the end index of the subarray
     }
 
     // If the subarray sum becomes negative or zero, reset the sum and move the start point
     if (intervalSum <= 0) {
       intervalSum = 0; // Reset the interval sum
-      start = i + 1; // Set the start to the next index
     }
   }
 
+  if (maxSum < 0) {
+    maxSum = 0;
+  }
+
   // Output the indices of the subarray
-  console.log("Start: ", start, "End: ", end); // Print the start and end indices of the maximum subarray
+  console.log("Start: ", ansStart, "End: ", ansEnd); // Print the start and end indices of the maximum subarray
 
   return maxSum; // Return the maximum sum
 }
