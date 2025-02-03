efb1e0bd4ab64b12a8f381fec149610a698f039c
zeeshan-kryptomind <zeeshan.ahmed@kryptomind.com>
2025-02-04 01:43:53 +0500

    adding comments

--- Arrays/Product-Of-Array-Except-Self/Better.ts
+++ Arrays/Product-Of-Array-Except-Self/Better.ts
@@ -1,20 +1,23 @@
-// Time Complexcity BigO(N)
+// Time Complexity: O(N)
 
 let nums: number[] = [1, 2, 3, 4];
 
 function productOfArray(nums: number[]) {
+  // Initialize the answer array with 1s
   let ansArray = new Array(nums.length).fill(1);
-  let prefix = 1;
-  let suffix = 1;
+  let prefix = 1; // Initialize prefix product
+  let suffix = 1; // Initialize suffix product
 
+  // Calculate prefix products and store in ansArray
   for (let i = 0; i < nums.length; i++) {
-    ansArray[i] = prefix;
-    prefix *= nums[i];
+    ansArray[i] = prefix; // Set the current index to prefix product
+    prefix *= nums[i]; // Update the prefix product
   }
 
+  // Calculate suffix products and multiply with current value in ansArray
   for (let j = nums.length - 1; j >= 0; j--) {
-    ansArray[j] *= suffix;
-    suffix *= nums[j];
+    ansArray[j] *= suffix; // Multiply the current index with suffix product
+    suffix *= nums[j]; // Update the suffix product
   }
 
   return ansArray; // Return the array with the products
