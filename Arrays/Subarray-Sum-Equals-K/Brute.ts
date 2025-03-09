96a5b3aae73f376e4a64d349cb61a2359913b114
zeeshan-kryptomind <zeeshan.ahmed@kryptomind.com>
2025-03-10 00:21:42 +0500

    adding subarray sum equals to k brute approach

---
+++ Arrays/Subarray-Sum-Equals-K/Brute.ts
@@ -0,0 +1,24 @@
+// Time complexcity: BigO(N^2)
+
+let nums: number[] = [1, 1, 1];
+
+let k = 2;
+function subArraySum(nums: number[]) {
+  let totalSubArrays = 0;
+
+  for (let i = 0; i < nums.length; i++) {
+    let sum = 0;
+    for (let j = i; j < nums.length; j++) {
+      // Inner loop first iteration it will be 1 and 2nd it will be 2 and in 3rd iteration it will be 3
+      sum += nums[j];
+
+      if (sum === k) {
+        //if sum is equal to k increment the count
+        totalSubArrays++;
+      }
+    }
+  }
+  return totalSubArrays;
+}
+
+console.log(subArraySum(nums));
